7a88dee181ecae09afd87a2616285625bfab41a4
Chen Yufei <cyfdecyf@gmail.com>
2020-10-18 03:14:14 +0800

    copy-gps support multiple dst.

--- copy-gps.sh
+++ copy-gps.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-if [[ $# != 2 ]]; then
-    echo "Usage: $(basename $0) <src> <dst>"
+set -e
+
+if (( $# < 2 )); then
+    echo "Usage: $(basename $0) <src> <dst> [dst2 [dst3 ...]]"
     exit 1
 fi
 
-src=$1
-dst=$2
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# -GPSPosition not writable.
-#exiftool -a -ee -tagsFromFile "$src" \
-#    -QuickTime:GPSCoordinates -GPSPosition $dst
+src="$1"
 
-exiftool -a -ee -tagsFromFile "$src" \
-    -GPSCoordinates -GPSAltitude -GPSAltitudeRef \
-    -GPSLatitude -GPSLongitude "$dst"
+copy_gps_tag() {
+    local dst="$1"
 
-# If GPSCoordinates does not exists, add it.
-# This happens if src is an jpg file which does not have GPSCoordinates 
-if ! exiftool -GPSCoordinates "$dst" | grep -q Coordinates; then 
-    add-gps-coordinates.sh "$dst"
-fi
+    # -GPSPosition not writable.
+    #exiftool -a -ee -tagsFromFile "$src" \
+    #    -QuickTime:GPSCoordinates -GPSPosition $dst
+
+    exiftool -a -ee -tagsFromFile "$src" \
+        -GPSCoordinates -GPSAltitude -GPSAltitudeRef \
+        -GPSLatitude -GPSLongitude "$dst"
+
+    # If GPSCoordinates does not exists, add it.
+    # This happens if src is an jpg file which does not have GPSCoordinates 
+    if ! exiftool -GPSCoordinates "$dst" | grep -q Coordinates; then
+        "$DIR/add-gps-coordinates.sh" "$dst"
+    fi
+}
+
+shift 1
+while (( $# > 0 )); do
+    copy_gps_tag "$1"
+    shift 1
+done
